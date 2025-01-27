0d87b83eeddd8b7498fcd56731660f6c5c71111a
Vladimir Mishin <bloodimirg@gmail.com>
2025-01-28 01:54:57 +0300

    Класс основного файла с ботом

---
+++ bot/cashbot.py
@@ -0,0 +1,45 @@
+import logging
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from dotenv import load_dotenv
+
+from bot.database.orm.users import add_user
+from bot.handlers.callback_handlers import CallbackHandlers
+from bot.handlers.bond_handlers import BondHandlers
+from bot.config import Base, engine
+from bot.keyboards.menu import Keyboard
+
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+
+
+class CashBot:
+    """Основной бот"""
+    def __init__(self):
+        self.bot = Bot(os.getenv('API_TOKEN'))
+        self.dp = Dispatcher()
+        self.keyboard = Keyboard()
+        self.bond_handlers = BondHandlers(self.dp, self.bot)  # Создаем объект BondHandlers
+        self.setup_handlers()
+
+    def setup_handlers(self):
+        """Регистрация глобальных обработчиков"""
+        self.dp.message.register(self.start_command, Command("start"))
+        BondHandlers(self.dp, self.bot)
+        CallbackHandlers(self.dp, self.bot, self.bond_handlers)
+
+    async def start_command(self, message: Message):
+        """Обрабочик команды /start"""
+        telegram_id = message.from_user.id
+        username = message.from_user.username
+        full_name = message.from_user.full_name
+        add_user(telegram_id, username, full_name)
+        await message.answer("Добро пожаловать!", reply_markup=self.keyboard.main_menu())
+
+    async def on_start(self):
+
+        Base.metadata.create_all(bind=engine)
+        await self.dp.start_polling(self.bot)
