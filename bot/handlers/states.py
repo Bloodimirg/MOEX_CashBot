625b823b2bb80d3261bb2101846978d77b3c8abc
Vladimir Mishin <bloodimirg@gmail.com>
2025-02-01 15:53:27 +0300

    Добавлено состояние для удаления

--- bot/handlers/states.py
+++ bot/handlers/states.py
@@ -3,4 +3,5 @@ from aiogram.fsm.state import State, StatesGroup
 
 class BondStates(StatesGroup):
     """Состояние ожидания получения тикера облигации."""
-    waiting_for_bond_ticker = State()
+    waiting_add_bond_ticker = State()
+    waiting_remove_bond_ticker = State()
